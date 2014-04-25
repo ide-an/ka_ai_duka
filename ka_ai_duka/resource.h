41c9e55301edf580054dd8e7f1d114bcc9ccdc03
ide-an <ideanweb@gmail.com>
2014-04-25 13:29:53 +0900

    アイコン変更

---
+++ ka_ai_duka/resource.h
@@ -0,0 +1,14 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define ID_EDIT_SCRIPTPATH_1P        101
+#define ID_EDIT_SCRIPTPATH_2P        102
+#define ID_GROUPBOX_1P        103
+#define ID_GROUPBOX_2P        104
+#define ID_ENABLED_1P    105
+#define ID_ENABLED_2P    106
+#define ID_SELECTFILE_1P    107
+#define ID_SELECTFILE_2P    108
+#define ID_EDIT_EXEPATH      122
+#define ID_SELECTFILE_EXE    123
