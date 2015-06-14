ffb6ddaccdc679b96c3d0147cacafaaed7eaff20
ide-an <ideanweb@gmail.com>
2015-06-15 00:05:40 +0900

    add debug option checkboxes

--- ka_ai_duka_w/resource.h
+++ ka_ai_duka_w/resource.h
@@ -1,14 +1,28 @@
-#ifndef IDC_STATIC
-#define IDC_STATIC (-1)
-#endif
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ka_ai_duka_w.rc
+//
+#define ID_EDIT_SCRIPTPATH_1P           101
+#define ID_EDIT_SCRIPTPATH_2P           102
+#define ID_GROUPBOX_1P                  103
+#define ID_GROUPBOX_2P                  104
+#define ID_ENABLED_1P                   105
+#define ID_ENABLED_2P                   106
+#define ID_SELECTFILE_1P                107
+#define ID_SELECTFILE_2P                108
+#define ID_EDIT_EXEPATH                 122
+#define ID_SELECTFILE_EXE               123
+#define IDC_CHECK1                      1000
+#define IDC_CHECK2                      1001
+#define IDC_STATIC                      -1
 
-#define ID_EDIT_SCRIPTPATH_1P        101
-#define ID_EDIT_SCRIPTPATH_2P        102
-#define ID_GROUPBOX_1P        103
-#define ID_GROUPBOX_2P        104
-#define ID_ENABLED_1P    105
-#define ID_ENABLED_2P    106
-#define ID_SELECTFILE_1P    107
-#define ID_SELECTFILE_2P    108
-#define ID_EDIT_EXEPATH      122
-#define ID_SELECTFILE_EXE    123
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
